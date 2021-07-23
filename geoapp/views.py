52d46451a0afe13ad879f591ae56a2270841ae4a
Quincewood <87698567+Quincewood@users.noreply.github.com>
2021-07-23 13:57:45 +0300

    Second commit

--- geoapp/views.py
+++ geoapp/views.py
@@ -7,12 +7,12 @@ def home(request):
     crd_dir = os.path.join(os.getcwd(), 'media', 'crd')
     m = folium.Map(location=[-6.161184, 35.745426], height=2000)
 
-    folium.GeoJson(os.path.join(crd_dir, 'data.geojson'), name='data', popup=folium.GeoJsonPopup(fields=["Name of respondent"] + ["Region"] + ["Ward"])).add_to(m)
+    folium.GeoJson(os.path.join(crd_dir, 'dealers.geojson'), name='dealers', popup=folium.GeoJsonPopup(fields=["Name of respondent"] + ["Region"] + ["Ward"])).add_to(m)
 
 
     m = m._repr_html_()
     context = {'my_map': m}
-    return render(request, 'geoApp/home.html', context)
+    return render(request, 'geoapp/home.html', context)
 
 
 
