1de34bde807bad8021661394d88a1cc0472e54bd
Quincewood <87698567+Quincewood@users.noreply.github.com>
2021-07-23 13:32:05 +0300

    Second commit

---
+++ geoapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+import os
+import folium
+
+
+def home(request):
+    crd_dir = os.path.join(os.getcwd(), 'media', 'crd')
+    m = folium.Map(location=[-6.161184, 35.745426], height=2000)
+
+    folium.GeoJson(os.path.join(crd_dir, 'data.geojson'), name='data', popup=folium.GeoJsonPopup(fields=["Name of respondent"] + ["Region"] + ["Ward"])).add_to(m)
+
+
+    m = m._repr_html_()
+    context = {'my_map': m}
+    return render(request, 'geoApp/home.html', context)
+
+
+
